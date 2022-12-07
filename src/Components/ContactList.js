f5ad10c319b40c6659d1f412884a5a3ef106406f
Muhammad Yasir <muhammadyasir@pk.folio3.com>
2022-12-07 19:27:56 +0500

    Contact List Component Updated

--- src/Components/ContactList.js
+++ src/Components/ContactList.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
 
 const ContactList = () => {
   return (
-    <div className='App-contact-list'>ContactList</div>
-  )
-}
+    <div className="App-contact-list">
+      <h2>All Contacts</h2>
 
-export default ContactList
+      <table className="table table-striped">
+        
+        <thead>
+          <tr>
+            <th scope="col">Sr Number</th>
+            <th scope="col">Name</th>
+            <th scope="col">Contact Number</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default ContactList;
