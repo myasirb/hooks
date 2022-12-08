170a5d89f7d9efd58df7858ea473b7294f24b49c
Muhammad Yasir <muhammadyasir@pk.folio3.com>
2022-12-08 17:56:08 +0500

    registered hook to context

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import "./App.css";
 import Home from "./components/Home";
+import { useContact } from "./hooks/contact-hook";
+import { ContactContext } from "./context/ContactContext";
 
 const App = () => {
+  const { list, push, pop } = useContact();
+
   return (
-    <div className="App">
-      <header className="App-header">Contact Book</header>
+    <ContactContext.Provider value={{ list: list, push: push, pop: pop }}>
+      <div className="App">
+        <header className="App-header">Contact Book</header>
 
-      <main>
-        <Home />
-      </main>
+        <main>
+          <Home />
+        </main>
 
-      <footer className="App-footer">Developed By Muhammad Yasir</footer>
-    </div>
+        <footer className="App-footer">Developed By Muhammad Yasir</footer>
+      </div>
+    </ContactContext.Provider>
   );
 };
 
