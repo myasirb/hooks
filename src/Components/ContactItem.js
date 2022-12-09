ca5c27c35813e251935fcee2f1f45986b6e9eccc
Muhammad Yasir <muhammadyasir@pk.folio3.com>
2022-12-09 18:48:00 +0500

    Delete contact handler updated

--- src/Components/ContactItem.js
+++ src/Components/ContactItem.js
@@ -2,13 +2,17 @@ import React, { useContext } from "react";
 import { ContactContext } from "../context/ContactContext";
 
 const ContactItem = ({contact, index}) => {
+  // useContect to get contacts and access function
   const contacts = useContext(ContactContext);
+
+  // Delete contact handler that calls pop method from context and pass contact to it. 
   const deleteContactHandler = (e) =>{
     e.preventDefault();
 
     contacts.pop(contact);
 
   }
+  
   return (
       <tr>
         <th scope="row">{index}</th>
