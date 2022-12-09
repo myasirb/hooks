3364f1eef345e1b27e05a75f0c5b8b14d4cebfca
Muhammad Yasir <muhammadyasir@pk.folio3.com>
2022-12-09 18:42:00 +0500

    custom hook useContact created

--- src/hooks/contact-hook.js
+++ src/hooks/contact-hook.js
@@ -1,23 +1,29 @@
 import { useCallback, useMemo, useState } from "react";
 
 export const useContact = () => {
+
+  // State to save list of contacts
   const [list, setList] = useState([]);
 
+  // call back method to add a new contact in the list
   const push = useCallback((newContact) => {
     setList((prevList) => {
       return prevList.concat(newContact);
     });
   }, []);
 
+  // call back method to remove a contact from list
   const pop = useCallback((removeContact) => {
     setList((prevList) => {
       return prevList.filter((contact) => contact.number !== removeContact.number);
     });
   }, []);
 
+  // memo to get count of contacts each time list state is updated
   const count = useMemo(()=>{
     return list.length;
   },[list])
 
+  // return 
   return {list, count, push, pop};
 };
