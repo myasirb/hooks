9f9c08bdec96756bb61896cef9cb8aae2c4734cc
Muhammad Yasir <muhammadyasir@pk.folio3.com>
2022-12-09 18:45:10 +0500

    Component updated

--- src/Components/ContactList.js
+++ src/Components/ContactList.js
@@ -1,10 +1,13 @@
 import React from "react";
 import ContactItem from "./ContactItem";
 
-const ContactList = ({ contacts, count }) => {
+import "./css/ContactList.css";
 
+const ContactList = ({ contacts, count }) => {
+  // To display serial number of contacts
   let countContacts = 1;
 
+  // if no contacts added
   if (!contacts || contacts.length === 0) {
     return <h1>No Contacts To Show</h1>;
   }
@@ -13,22 +16,32 @@ const ContactList = ({ contacts, count }) => {
     <div className="App-contact-list">
       <h2>Total Contacts {count} </h2>
 
-      <table className="table table-striped">
-        <thead>
-          <tr>
-            <th scope="col">Sr Number</th>
-            <th scope="col">Name</th>
-            <th scope="col">Contact Number</th>
-            <th scope="col">Actions</th>
-          </tr>
-        </thead>
+      <div className="App-contact-list-table">
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th scope="col">Sr Number</th>
+              <th scope="col">Name</th>
+              <th scope="col">Contact Number</th>
+              <th scope="col">Actions</th>
+            </tr>
+          </thead>
 
-        <tbody>
-          {contacts.map((contact) => {
-            return <ContactItem key={++countContacts} contact={contact} index = {countContacts} />;
-          })}
-        </tbody>
-      </table>
+          <tbody>
+            {/* For each contact in contacts received as prop from parent function, show contact item */}
+
+            {contacts.map((contact) => {
+              return (
+                <ContactItem
+                  key={++countContacts}
+                  contact={contact}
+                  index={countContacts}
+                />
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
